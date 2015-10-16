8097f9625261c5ae2f7fb569aac2a079b657dc9a
hoangna <nahoang9698@gmail.com>
2015-10-16 14:09:20 +0700

    add new solution

---
+++ 20Recipes/20Recipes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_20Recipes.MvcApplication" Language="C#" %>
---
+++ 20Recipes/20Recipes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_20Recipes.Startup))]
+namespace _20Recipes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
