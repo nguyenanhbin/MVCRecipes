f9651afe939b4e10fc24543258aa6016b16a9ed2
hoangna <nahoang9698@gmail.com>
2015-10-16 14:25:38 +0700

    test github02

--- 20Recipes/20Recipes/Controllers/HomeController.cs
+++ 20Recipes/20Recipes/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace _20Recipes.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Test Github";
+            ViewBag.Message = "Test Github 02";
             return View();
         }
 
