f64dd2ae7f40d58b6bd01de24400c102b50e0f02
hoangna <nahoang9698@gmail.com>
2015-10-19 10:20:42 +0700

    add model and controller

---
+++ 20Recipes/20Recipes/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace _20Recipes.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Isbn { get; set; }
+        public string Summary { get; set; }
+        public string Author { get; set; }
+        public string Thumbnail { get; set; }
+        public double Price { get; set; }
+        public DateTime Published { get; set; }
+    }
+
+    public class BookDBContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
